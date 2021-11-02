424d017b33db86305cf60a0165076cf18ad862d8
Ava Beaver <zanbeaver@gmail.com>
2021-11-02 10:04:55 -0700

    Initial project commit

---
+++ creatures/CreatureSprite.gd
@@ -0,0 +1,21 @@
+extends AnimatedSprite
+
+class_name CreatureSprite
+
+func animate_take_damage():
+	pass
+
+func animate_stun():
+	pass
+
+func animate_unstun():
+	pass
+
+func animate_explode():
+	pass
+
+func animate_fade_away():
+	pass
+
+func animate_disintegrate():
+	pass
