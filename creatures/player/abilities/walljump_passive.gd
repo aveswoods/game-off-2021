d5fc0534cfddff523a769339f81ef15dd52db21b
Ava Beaver <zanbeaver@gmail.com>
2021-11-05 18:26:35 -0700

    Added walljump

---
+++ creatures/player/abilities/walljump_passive.gd
@@ -0,0 +1,21 @@
+extends RayCast2D
+
+var _player = null
+
+
+func equip(player):
+	_player = player
+	_player.add_child(self)
+
+
+func _physics_process(_delta):
+	if Input.is_action_just_pressed("ui_up") and not _player.is_on_floor():
+		if _player.is_facing_left():
+			cast_to.x *= -1
+			force_raycast_update()
+			if is_colliding():
+				_player.bump(Vector2(-1 * _player.jump_impulse, _player.jump_impulse))
+			cast_to.x *= -1
+		else:
+			if is_colliding():
+				_player.bump(Vector2(_player.jump_impulse, _player.jump_impulse))
--- creatures/test_scene.gd
+++ creatures/test_scene.gd
@@ -4,11 +4,13 @@ var lunge_action = load("res://creatures/player/abilities/lunge_action.tscn").in
 var bite_action = load("res://creatures/player/abilities/bite_action.tscn").instance()
 var stun_passive = load("res://creatures/player/abilities/stomp_passive.tscn").instance()
 var doublejump_passive = load("res://creatures/player/abilities/doublejump_passive.gd").new()
+var walljump_passive = load("res://creatures/player/abilities/walljump_passive.tscn").instance()
 
 func _ready():
 	lunge_action.equip($Player)
 	bite_action.equip($Player)
 	stun_passive.equip($Player)
 	doublejump_passive.equip($Player)
+	walljump_passive.equip($Player)
 	$Player.action1_script = lunge_action
 	$Player.action2_script = bite_action
