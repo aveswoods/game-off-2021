c78722661e2cb41a58893f62fd6b28c49dc45f66
Ava Beaver <zanbeaver@gmail.com>
2021-11-05 18:47:21 -0700

    Added glide passive

---
+++ creatures/player/abilities/glide_passive.gd
@@ -0,0 +1,32 @@
+extends Node
+
+const fall_speed = 30
+const acceleration = 0.95
+var _player = null
+var _player_acceleration = 0
+var _gliding = false
+
+
+func equip(player):
+	_player = player
+	_player.add_child(self)
+
+
+func _ready():
+	_player.connect("collided_with_floor", self, "_on_player_hits_floor")
+
+
+func _physics_process(_delta):
+	if Input.is_action_pressed("ui_up") and not _player.is_on_floor() and _player.velocity.y > 0:
+		_player.velocity.y = fall_speed
+		if not _gliding:
+			_player_acceleration = _player.acceleration
+			_player.acceleration = acceleration
+			_gliding = true
+	elif Input.is_action_just_released("ui_up") and _gliding:
+		_player.acceleration = _player_acceleration
+		_gliding = false
+
+func _on_player_hits_floor():
+	_player.acceleration = _player_acceleration
+	_gliding = false
--- creatures/test_scene.gd
+++ creatures/test_scene.gd
@@ -5,12 +5,14 @@ var bite_action = load("res://creatures/player/abilities/bite_action.tscn").inst
 var stun_passive = load("res://creatures/player/abilities/stomp_passive.tscn").instance()
 var doublejump_passive = load("res://creatures/player/abilities/doublejump_passive.gd").new()
 var walljump_passive = load("res://creatures/player/abilities/walljump_passive.tscn").instance()
+var glide_passive = load("res://creatures/player/abilities/glide_passive.gd").new()
 
 func _ready():
 	lunge_action.equip($Player)
 	bite_action.equip($Player)
 	stun_passive.equip($Player)
-	doublejump_passive.equip($Player)
-	walljump_passive.equip($Player)
+	#doublejump_passive.equip($Player)
+	#walljump_passive.equip($Player)
+	glide_passive.equip($Player)
 	$Player.action1_script = lunge_action
 	$Player.action2_script = bite_action
